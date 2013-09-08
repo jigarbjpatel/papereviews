e1b2ffc4d37b5c5e7ba34370d0a88a45de951b9a
Jigar Patel <jigarbjpatle@gmail.com>
2013-09-07 23:57:37 -0400

    Devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Papereviews::Application.routes.draw do
+  devise_for :users
+  resources :users
+  get "reviews/index"
+  get "reviews/new"
+  root :to => "reviews#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,6 @@ Papereviews::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  
 end
